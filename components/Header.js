1a2093ffb69678634e2a93254aeef8ec3cb0bd6c
Official-Divyansh <107239607+Official-Divyansh@users.noreply.github.com>
2022-07-04 18:29:39 +0530

    Add files via upload

--- components/Header.js
+++ components/Header.js
@@ -38,7 +38,7 @@ function Header({placeholder}) {
 
   return (
     <header className='sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10'>
-        <div className='relative flex items-center h-10 cursor-pointer my-auto'
+        <div className='relative flex items-center  h-10 cursor-pointer my-auto'
        >
         <Image
         alt='Header'
@@ -52,33 +52,32 @@ function Header({placeholder}) {
         
         </div>
 
-        <div className='flex items-center md:border-2 rounded-full py-2 md:shadow-sm'>
+        <div className='flex items-center md:border-2 rounded-full py-2 md:shadow-sm '>
             <input type={search} placeholder={ placeholder || "Start your search"}
             onChange={(e)=> setSearch(e.target.value)}
-            className='flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-600' />
+            className='flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-600 ' />
             <SearchIcon className='hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2' />
         </div>
 
-        <div className='flex items-center space-x-4 justify-end text-gray-500'>
+        <div className=' hidden sm:flex items-center space-x-4 justify-end text-gray-500'>
             <p className='hidden md:inline'> Become a Host</p>
-            <GlobeAltIcon className='h-6 cursor-pointer' />
+            <GlobeAltIcon className='h-6 cursor-pointer hidden md:block' />
             
-            <div className='flex items-center space-x-2 border-2 p-2 rounded-full'>
+            <div className='flex items-center space-x-0 border-2 p-2 rounded-full'>
                <MenuIcon className='h-6' />
                <UserCircleIcon className='h-6' />
             </div>
         </div>
         {
           search && (
-            <div className='flex flex-col col-span-3 mx-auto'>
-
+            <div className='bg-white  absolute sm:static top-[2cm] flex flex-col col-span-3 mx-auto'>
             <DateRangePicker
             ranges={[selectionRange]}
             minDate={new Date()}
             rangeColors={["#FD5B61"]}
             onChange={handleSelect}
             />
-            <div className='flex items-center border-b mb-4'>
+            <div className=' flex items-center border-b mb-4'>
             <h2 className='text-2xl flex-grow font-semibold'>
             Number of Guests
             </h2>
