60886c191b6fbea59452f22bb1d3dfa2d53f0708
Official-Divyansh <107239607+Official-Divyansh@users.noreply.github.com>
2022-07-04 18:54:36 +0530

    Add files via upload

--- components/Header.js
+++ components/Header.js
@@ -53,19 +53,19 @@ function Header({placeholder}) {
         </div>
 
         <div className='flex items-center md:border-2 rounded-full py-2 md:shadow-sm '>
-            <input type={search} placeholder={ placeholder || "Start your search"}
+            <input type={search} placeholder="Start your search"
             onChange={(e)=> setSearch(e.target.value)}
             className='flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-600 ' />
             <SearchIcon className='hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2' />
         </div>
 
-        <div className=' hidden sm:flex items-center space-x-4 justify-end text-gray-500'>
+        <div className=' flex items-center space-x-4 justify-end text-gray-500'>
             <p className='hidden md:inline'> Become a Host</p>
             <GlobeAltIcon className='h-6 cursor-pointer hidden md:block' />
             
             <div className='flex items-center space-x-0 border-2 p-2 rounded-full'>
-               <MenuIcon className='h-6' />
-               <UserCircleIcon className='h-6' />
+               <MenuIcon className='  h-6' />
+               <UserCircleIcon className='hidden sm:flex h-6' />
             </div>
         </div>
         {
